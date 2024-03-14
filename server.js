a0eae9a6b94222bb76f67e87e132c3f9b79f1dea
Alexander Doja <alexander.doja@outlook.com>
2024-03-14 10:50:46 +0100

    created send endpoint

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json()); // Parsa JSON-baserade inkommande förfrågningar
 
 // Konstanter för Notion API
 const NOTION_API_BASE_URL = 'https://api.notion.com/v1'; // Bas URL för Notion API
-const NOTION_API_KEY = 'NOTION API NYCKEL'; // Din Notion API-nyckel (bör hanteras säkrare i praktiken)
+const NOTION_API_KEY = 'secret_8ESoOICHaglvbEsc0AI4LYanbfQdVEsNdEXWdHN7pp4'; // Din Notion API-nyckel (bör hanteras säkrare i praktiken)
 const NOTION_DATABASE_IDS = ['0cb39c6042c547a988326a168176d6fe', 'b86de2e54b4c4e7789b07dc308489e4d', 'fba06eb24c9241e585ab41aaae362e31'];
 
 // Definiera en POST endpoint för att interagera med Notion API
@@ -24,6 +24,27 @@ app.post('/api/notion', async (req, res) => {
         }
       })
     );
+
+    const responses = await Promise.all(promises); 
+
+    const aggregatedData = responses.map(response => response.data);
+
+    res.json(aggregatedData);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Definiera en POST endpoint för att interagera med Notion API
+app.post('/api/notion/send', async (req, res) => {
+  try {
+    axios.post(`${NOTION_API_BASE_URL}/pages`, req.body, {
+      headers: {
+        'Authorization': `Bearer ${NOTION_API_KEY}`,
+        'Notion-Version': '2022-06-28',
+      }
+    });
 
     const responses = await Promise.all(promises); 
 
